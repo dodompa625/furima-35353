ddf67e8e4d0bef5f365a137ca2accaa3ca41507f
dodompa625 <dodompa625@gmail.com>
2021-09-02 16:42:44 +0900

    商品詳細表示機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,10 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+  
+  end
+
   private
   def product_params
     params.require(:product).permit(:product_image, :product_name, :text, :price, :text, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_date_id).merge(user_id: current_user.id)
